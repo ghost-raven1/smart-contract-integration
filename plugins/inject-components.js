7b4e137d3f165658819adae4733fb45059cfddff
ghost-raven1 <sonofagronom@gmail.com>
2021-08-03 19:42:54 +0700

    Initial commit

---
+++ plugins/inject-components.js
@@ -0,0 +1,13 @@
+// import Vue from 'vue'
+
+// import BaseModalContainer from '~/components/BaseModalContainer'
+// import BaseModalBox from '~/components/BaseModalBox'
+// import LoaderScreen from '~/components/LoaderScreen'
+// import BaseArticleNavigation from '~/components/ui/BaseArticleNavigation'
+// import BaseDD from '~/components/ui/BaseDD'
+//
+// Vue.component('base-modal-container', BaseModalContainer)
+// Vue.component('base-modal-box', BaseModalBox)
+// Vue.component('loader-screen', LoaderScreen)
+// Vue.component('base-article-navigation', BaseArticleNavigation)
+// Vue.component('base-dd', BaseDD)
